a743f775d100be22774c970be0d3398d0b174440
Suin Park <96513157+tndls2@users.noreply.github.com>
2024-06-20 20:35:14 +0900

    Update 베르트랑 공준.py

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,11 +1,10 @@
 n=123456  # n은 123,456 이하의 정수
 ''' 에라토스테네스의 체 이용하여 소수 구하기 '''
-a = [False,False] + [True]*(2*n-1)
-ls_primes=[]  # 소수 리스트
+ls_prime = [False,False] + [True]*(2*n-1)  # 소수 여부 리스트
 for i in range(2,2*n+1):
-  if a[i]:
+  if ls_prime[i]:
     for j in range(2*i, 2*n+1, i):
-        a[j] = False
+        ls_prime[j] = False
 
 while 1:
     n = int(input())
@@ -14,6 +13,6 @@ while 1:
     cnt = 0
     for i in range(n+1, 2*n+1):
         # n~2*n 범위 안에 있는 소수 세기
-        if a[i]==True:
+        if ls_prime[i]==True:
             cnt +=1
     print(cnt)
