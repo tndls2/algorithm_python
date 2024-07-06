c0201d18d6654cf37a105e505b3315fd57a01fb8
Suin Park <96513157+tndls2@users.noreply.github.com>
2024-07-06 14:09:14 +0900

    [Silver III] Title: 칸토어 집합, Time: 708 ms, Memory: 81372 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -0,0 +1,33 @@
+import math
+import sys
+
+def cantor_set(start, length, blank_set):
+    if length == 1:
+        return
+    
+    split_length = length // 3  # 3등분
+    mid_start = start + split_length
+    mid_end = start + 2 * split_length
+    
+    blank_set.update(range(mid_start, mid_end))  # 중간 문자열은 공백이 됨
+    
+    cantor_set(start, split_length, blank_set)  # 첫번째 문자열
+    cantor_set(mid_end, split_length, blank_set)  # 마지막 문자열
+
+while True:
+    try:
+        n = int(input().strip())
+        length = int(math.pow(3, n))  # 3^n개의 -로 이뤄진 문자열
+        line = '-' * length  # 초기 문자열
+        blank_set = set()  # 공백이 될 문자열 요소
+        cantor_set(0, length, blank_set)
+        
+        # 결과 출력
+        for i in range(length):
+            if i in blank_set:
+                print(" ", end="")
+            else:
+                print("-", end="")
+        print()  # 줄바꿈
+    except EOFError:
+        break
