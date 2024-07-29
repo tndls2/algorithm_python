3ec3a2fea552efb0cc711c59c1fd511975ae9f09
Suin Park <96513157+tndls2@users.noreply.github.com>
2024-07-29 15:34:49 +0900

    Update 연속합.py

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,6 +1,10 @@
 import sys
+
 n = int(sys.stdin.readline().rstrip())
 sequence_ls = list(map(int, sys.stdin.readline().split()))
+
 for i in range(len(sequence_ls)-1):
+    # 이전 원소들과의 최대 합 vs 현재 원소값 중 최댓값으로 update
     sequence_ls[i+1] = max(sequence_ls[i]+sequence_ls[i+1], sequence_ls[i+1])
+    
 print(max(sequence_ls))
