ab5e1aaac51d33d16113e8a76497aa47d43570d6
Suin Park <96513157+tndls2@users.noreply.github.com>
2024-08-01 13:54:00 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 184 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,9 @@
+import sys
+n = int(input())
+sequence_ls = list(map(int, sys.stdin.readline().split()))
+dp = [1] * n # 첫 번째 요소 ~ 각 i번째 요소까지에 대해서 최장부분수열의 길이 : 1로 기본 세팅
+for i in range(1, n):
+    for j in range(i):
+        if sequence_ls[j] < sequence_ls[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(max(dp))  # n번째 요소까지수의 수열에 대한 최장부분수열의 길이 출력
