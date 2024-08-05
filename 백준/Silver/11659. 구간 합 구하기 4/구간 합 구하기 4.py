8d558aae1065774442edd41a317f79b45801b0d7
Suin Park <96513157+tndls2@users.noreply.github.com>
2024-08-05 11:11:04 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 252 ms, Memory: 41116 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,15 @@
+import sys
+import itertools
+
+n, m = map(int, sys.stdin.readline().split())
+n_ls = list(map(int, sys.stdin.readline().split()))
+accumulate_ls = [0] + list(itertools.accumulate(n_ls))  # n_ls에 대한 부분 누적합 저장
+## 부분 누적합 -> itertools.accumulate 활용
+## accumulate([1,2,3,4,5]) → 1 3 6 10 15
+## accumulate([1,2,3,4,5], initial=100) → 100 101 103 106 110 115
+## accumulate([1,2,3,4,5], operator.mul) → 1 2 6 24 120
+        
+for _ in range(m):
+    start_index, end_index = map(int, sys.stdin.readline().split())
+    answer = accumulate_ls[end_index] - accumulate_ls[start_index-1]
+    print(answer)
