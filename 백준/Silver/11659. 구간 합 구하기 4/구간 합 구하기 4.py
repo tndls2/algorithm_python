65dc9ef4056dcc7c6ff092f2bac1059aba432096
Suin Park <96513157+tndls2@users.noreply.github.com>
2024-08-05 11:12:27 +0900

    Update 구간 합 구하기 4.py

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -2,6 +2,10 @@ import sys
 import itertools
 
 n, m = map(int, sys.stdin.readline().split())
+# 1 ≤ N ≤ 100,000 
+# 1 ≤ M ≤ 100,000
+# input() 사용시 시간 초과 발생
+
 n_ls = list(map(int, sys.stdin.readline().split()))
 accumulate_ls = [0] + list(itertools.accumulate(n_ls))  # n_ls에 대한 부분 누적합 저장
 ## 부분 누적합 -> itertools.accumulate 활용
